133d3b5aa2c79f4d13096bf7c7fd2323cacaf905
SpaceInstructor <gabriel.stiufliuc6@gmail.com>
2020-07-14 12:45:42 +0300

    Init

---
+++ ios/Pods/Target Support Files/Pods-NexusAuroraPlatform-tvOSTests/Pods-NexusAuroraPlatform-tvOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NexusAuroraPlatform_tvOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_NexusAuroraPlatform_tvOSTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-NexusAuroraPlatformTests/Pods-NexusAuroraPlatformTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NexusAuroraPlatformTests : NSObject
+@end
+@implementation PodsDummy_Pods_NexusAuroraPlatformTests
+@end
